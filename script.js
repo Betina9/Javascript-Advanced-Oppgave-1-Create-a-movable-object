cd6134d5dbbed74e92e37b4bd69b3da2c38e321e
Betina9 <ob042025@gmail.com>
2025-07-31 13:03:32 +0200

    la inn litt variabler og const

---
+++ script.js
@@ -0,0 +1,9 @@
+const player = document.getElementById("player");
+const target = document.getElementById("target");
+const scoreEl = document.getElementById("score");
+
+let score = 0;
+let posx = 100;
+let posy = 100;
+const step = 20;
+const playerSize = 40;
